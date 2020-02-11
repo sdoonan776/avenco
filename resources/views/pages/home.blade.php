e52ddac2b071adb9262e01593e5118da67eef69d
Stuart Doonan <studoonan@yahoo.co.uk>
2020-02-11 14:54:29 +0000

    Update docker config

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,22 +3,7 @@
 @section('title', 'Home')
 
 @section('main')
-    <section class="hero-slider">
-        <div class="hero-items owl-carousel">
-            <div class="single-slider-item set-bg" style="{{ asset('resources/assets/img/slider-1.jpg'
-            ) }}">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h1>2020</h1>
-                            <h2>Lookbook.</h2>
-                            <a href="{{ route('products.index') }}" class="primary-btn">See More</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <div id="app"></div>
    
     <section class="features-section spad">
         <div class="features-ads">
